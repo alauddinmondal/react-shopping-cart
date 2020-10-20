9cb85530033e25e7e7451189416699c05458ea4b
Your Name <arshialauddin@gmail.com>
2020-10-20 23:06:04 +0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React shopping cart</a>
+      </header>
+      <main>
+        React product list
+      </main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
